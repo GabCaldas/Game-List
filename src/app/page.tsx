97d0b63d665a1af7925eeb49b131e9f6e84a77fe
GabCaldas <gabrielcaldas7@gmail.com>
2023-06-23 17:02:19 -0300

    first commit no componentization

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+import MainPage from '../pages/MainPage';
+
+
+export default function HomeServer() {
+  return (
+    <MainPage/>
+  );
+}
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,14 @@
+'use clients'
+
+import GameList from '@/components/GameList';
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <GameList/>
+    </>
+  );
+};
+
+export default HomePage;
